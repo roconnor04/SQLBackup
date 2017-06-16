2d2d5b0eb3c4b3deac1beea6e90c3d569b37298f
Ryan O'Connor <roconnor04@gmail.com>
2017-06-16 01:22:14 +0100

    Added Data migration

--- AllDBBackup.ps1
+++ AllDBBackup.ps1
@@ -1,8 +1,11 @@
-﻿$path = "C:\Backups\Test\"
+﻿$path = "C:\Backups\Location\"
+$Username = "ExampleUser"
+$Password = ConvertTo-SecureString "ExamplePassword" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential($Username, $Password)
 
 Get-SqlDatabase -ServerInstance localhost |
 Where { $_.Name -ne 'tempdb' } | foreach{
-Backup-SqlDatabase -DatabaseObject $_ -BackupFile "C:\Backups\Test\$($_.NAME)_db_$(Get-Date -UFormat %Y%m%d%H%M).bak"}
+Backup-SqlDatabase -DatabaseObject $_ -BackupFile "C:\Backups\Location\$($_.NAME)_db_$(Get-Date -UFormat %Y%m%d%H%M).bak"}
 
 function create-7zip([String] $aDirectory, [String] $aZipfile){
     [string]$pathToZipExe = "C:\Program Files\7-zip\7z.exe";
@@ -10,7 +13,11 @@ function create-7zip([String] $aDirectory, [String] $aZipfile){
     & $pathToZipExe $arguments;
 }
 
-create-7zip "C:\Backups\Test\*"  "C:\Backups\Test\test.7z"
+create-7zip "C:\Backups\Location\*"  "C:\Backups\Location\Web_$(Get-Date -UFormat %Y%m%d%H%M).7z"
 
 
 get-childitem $path -include *.bak -recurse | foreach ($_) {remove-item $_.fullname}
+
+New-PSDrive –Name “K” –PSProvider FileSystem –Root “\\Final\Destination\DBs” –Credential $mycreds
+Move-Item -path "$path*.7z" -Destination K:
+Remove-PSDrive K
